393c56a06577f09c1f98a67a4b2b815898ff60b4
Karen Dashyan <dashyankaren@outlook.com>
2023-11-26 20:56:59 +0400

    Prime numbers Computing by using Parallelism

---
+++ IsPrimeByParallel/IsPrimeByParallel/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+
+namespace IsPrimeByParallel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ParallelCompute(100));
+        }
+
+        static int ParallelCompute(int x)
+        {
+            int sum = 0;
+
+            Parallel.For(0, x, i =>
+            {
+                if (IsPrime(i))
+                {
+                    sum += i;
+                }
+            });
+            return sum;
+           
+
+           
+        }
+        static bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            if (number == 2)
+                return true;
+
+            if (number % 2 == 0)
+                return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnderstandingPararell/Program.cs
@@ -0,0 +1,64 @@
+﻿//#define TRACE
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BarrierSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 2_000_00000);
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            NonParallelCompute(numbers);
+            sw.Stop();
+            Console.WriteLine("Non Parallel {0}", sw.ElapsedMilliseconds);
+            sw.Reset();
+            sw.Start();
+            ParallelCompute(numbers);
+            sw.Stop();
+            Console.WriteLine("Parallel {0}", sw.ElapsedMilliseconds);
+        }
+
+        public static void NonParallelCompute(IEnumerable<int> numbers)
+        {
+            ConcurrentBag<int> ints = new ConcurrentBag<int>();
+            foreach (int i in numbers)
+            {
+                if (IsOdd(i))
+                {
+                    ints.Add(i);
+                }
+
+            };
+        }
+        public static void ParallelCompute(IEnumerable<int> numbers)
+        {
+            ConcurrentBag<int> ints = new ConcurrentBag<int>();
+            Parallel.ForEach(numbers, (e =>
+            {
+
+                if (IsOdd(e))
+                {
+                    ints.Add(e);
+                }
+
+            }));
+        }
+
+
+        public static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+    }
+}
